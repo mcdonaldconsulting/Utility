73bc8536f2643b667d0df2f1ea8d269227b88320
Patrick McDonald <paddymcdonald@gmail.com>
2012-03-22 00:57:30 +0000

    Created McDonald.Utility project

---
+++ McDonald.Utility/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace McDonald
+{
+    using System.Collections.Generic;
+
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool IsNotNullOrWhiteSpace(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        public static string Join(this IEnumerable<string> values, string separator)
+        {
+            return string.Join(separator, values);
+        }
+    }
+}
