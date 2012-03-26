15351bd48d70e6ab7dd1be9b68d791cd0e92bd1a
Patrick McDonald <paddymcdonald@gmail.com>
2012-03-26 21:29:15 +0100

    Added McDonaldConsulting.Utility.Tests

---
+++ McDonaldConsulting.Utility.Tests/QueryStringTests.cs
@@ -0,0 +1,44 @@
+﻿namespace McDonaldConsulting.Utility.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class QueryStringTests
+    {
+        [TestMethod]
+        public void Indexer()
+        {
+            var queryString = new QueryString();
+            queryString["a"] = "b";
+            Assert.AreEqual("b", queryString["a"]);
+        }
+
+        [TestMethod]
+        public void Enumerator()
+        {
+            var queryString = new QueryString();
+            queryString["a"] = "b";
+            queryString["c"] = "d";
+
+            var enumerator = queryString.GetEnumerator();
+            enumerator.MoveNext();
+            dynamic item1 = enumerator.Current;
+            Assert.AreEqual("a", item1.Key);
+            Assert.AreEqual("b", item1.Value);
+            enumerator.MoveNext();
+            dynamic item2 = enumerator.Current;
+            Assert.AreEqual("c", item2.Key);
+            Assert.AreEqual("d", item2.Value);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var queryString = new QueryString();
+
+            queryString.Add("q", "mcdonald");
+
+            Assert.AreEqual("q=mcdonald", queryString.ToString());
+        }
+    }
+}
---
+++ McDonaldConsulting.Utility.Tests/StringExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿namespace McDonaldConsulting.Utility.Tests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        private const string NullString = null;
+        private static readonly string emptyString = string.Empty;
+        private const string WhitespaceString = " \t \r \n ";
+        private const string McDonald = "McDonald";
+
+        [TestMethod]
+        public void IsNullOrEmpty()
+        {
+            Assert.IsTrue(NullString.IsNullOrEmpty());
+            Assert.IsTrue(emptyString.IsNullOrEmpty());
+            Assert.IsFalse(WhitespaceString.IsNullOrEmpty());
+            Assert.IsFalse(McDonald.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void IsNotNullOrEmpty()
+        {
+            Assert.IsFalse(NullString.IsNotNullOrEmpty());
+            Assert.IsFalse(emptyString.IsNotNullOrEmpty());
+            Assert.IsTrue(WhitespaceString.IsNotNullOrEmpty());
+            Assert.IsTrue(McDonald.IsNotNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void IsNullOrWhiteSpace()
+        {
+            Assert.IsTrue(NullString.IsNullOrWhiteSpace());
+            Assert.IsTrue(emptyString.IsNullOrWhiteSpace());
+            Assert.IsTrue(WhitespaceString.IsNullOrWhiteSpace());
+            Assert.IsFalse(McDonald.IsNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void IsNotNullOrWhiteSpace()
+        {
+            Assert.IsFalse(NullString.IsNotNullOrWhiteSpace());
+            Assert.IsFalse(emptyString.IsNotNullOrWhiteSpace());
+            Assert.IsFalse(WhitespaceString.IsNotNullOrWhiteSpace());
+            Assert.IsTrue(McDonald.IsNotNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Join()
+        {
+            // Arrange
+            object[] objects = { 1, 2, 3 };
+            string[] strings = { "a", "b", "c" };
+            var enumerable = new List<string> { "a", "b", "c" };
+
+            // Act & Assert
+            Assert.AreEqual("1,2,3", objects.Join(","));
+            Assert.AreEqual("a+b+c", strings.Join("+"));
+            Assert.AreEqual("a;b;c", enumerable.Join(";"));
+        }
+
+        [TestMethod]
+        public void FormatWith()
+        {
+            Assert.AreEqual("1: a", "1: {0}".FormatWith("a"));
+            Assert.AreEqual("2: a b", "2: {0} {1}".FormatWith("a", "b"));
+        }
+    }
+}
