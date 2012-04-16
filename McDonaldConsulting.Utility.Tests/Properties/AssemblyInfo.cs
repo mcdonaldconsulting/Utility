bf5fc299968801f8f580b7f8f7b08a845f149809
Patrick McDonald <paddymcdonald@gmail.com>
2012-04-16 23:48:46 +0100

    Added UppercuT

--- McDonaldConsulting.Utility.Tests/Properties/AssemblyInfo.cs
+++ McDonaldConsulting.Utility.Tests/Properties/AssemblyInfo.cs
@@ -1,35 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("McDonaldConsulting.Utility.Tests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("McDonald Consulting")]
-[assembly: AssemblyProduct("McDonaldConsulting.Utility.Tests")]
-[assembly: AssemblyCopyright("Copyright © McDonald Consulting 2012")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e1d8bf14-1199-4a1f-bb2c-7caa416e9dce")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- McDonaldConsulting.Utility/Properties/AssemblyInfo.cs
+++ McDonaldConsulting.Utility/Properties/AssemblyInfo.cs
@@ -5,32 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("McDonaldConsulting.Utility")]
-[assembly: AssemblyDescription("Basic .NET 4.0 Extension Methods")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("McDonald Consulting")]
-[assembly: AssemblyProduct("McDonaldConsulting.Utility")]
-[assembly: AssemblyCopyright("Copyright © McDonald Consulting 2012")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("07a24fd7-35a1-40d6-8624-c195a57eece3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
-
-[assembly: CLSCompliant(true)]
