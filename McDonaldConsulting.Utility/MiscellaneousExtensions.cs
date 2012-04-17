83bee7f7f1e1efb4a73dc9a259288e51a0b259b1
Patrick McDonald <paddymcdonald@gmail.com>
2012-04-17 12:27:16 +0100

    Added IComparable.IsBetween(lower, upper) extension method

---
+++ McDonaldConsulting.Utility/MiscellaneousExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace McDonaldConsulting
+{
+    using System;
+
+    public static class MiscellaneousExtensions
+    {
+        public static bool IsBetween<T>(this T value, T lower, T upper) where T : IComparable
+        {
+            return value.CompareTo(lower) >= 0 && value.CompareTo(upper) <= 0;
+        }
+    }
+}
